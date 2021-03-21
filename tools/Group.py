fbc688b9aa3733a5a6568e78a2617adab548cb2f
thekdorm <thekdorm@gmail.com>
2021-03-21 14:55:47 -0700

    Added commands for group management

--- tools/Group.py
+++ tools/Group.py
@@ -3,38 +3,70 @@ from typing import List
 
 class Group:
 
-    def __init__(self, owner: str, scheduled_time: str, composition: List[int], my_role: str):
+    def __init__(self, owner: str, name: str, my_role: str, group_type: str=None, 
+                    rating: int=None, scheduled_time: str=None, composition: List[int]=None):
         """Init method for Group
 
+        Intended to be used for either Arena 2v2, 3v3 or 10v10 BGs
+
         Args:
-            owner (str): Player name of person creating group
-            scheduled_time (str): Server time that group will start
-            composition (list): Tank/DPS/Healer i.e. for 1/3/6 -> [1, 3, 6]
-            my_role (str): The role that the creator will be playing for composition
+            owner (str): Player who created the group
+            name (str): Name of group for storing in GroupManager
+            my_role (str): Role of player who created group, <tank | healer | dps>
+            group_type (str, optional): <arena2 | arena3 | rbg>. Defaults to None.
+            rating (int, optional): Rating of group. Defaults to None.
+            scheduled_time (str, optional): If desired, specify a scheduled server time for group. Defaults to None.
+            composition (List[int], optional): Desired composition for group. Defaults to None.
         """
         
-        self.owner, self.time = owner, scheduled_time
+        self.owner, self.name, self.group_type, self._total = owner, name, group_type, 0
+        
+        if group_type == 'arena2':
+            self._max = 2
+        elif group_type == 'arena3':
+            self._max = 3
+        elif group_type == 'rbg':
+            self._max = 10
+        else:
+            self._max = 20  # for raids? TODO flesh this out later
+
+        if rating:
+            self.rating = rating
+        if scheduled_time:
+            self.time = scheduled_time
+        if composition:
+            tanks = composition[0]
+            healers = composition[1]
+            dps = composition[2]
+        else:
+            tanks = 10
+            healers = 10
+            dps = 10
+
         self.comp = {
                         'tank': 
                             {
-                                'number': composition[0],
+                                'number': tanks,
                                 'players': []
                             },
 
                         'healer':
                             {
-                                'number': composition[1],
+                                'number': healers,
                                 'players': []
                             },
 
                         'dps':
                             {
-                                'number': composition[2],
+                                'number': dps,
                                 'players': []
                             }
                     }  # type: ignore
-        
+
         self.add_member(owner, my_role)
+        
+        for role in self.comp:
+            self._total += len(self.comp[role]['players'])
 
     def add_member(self, member: str, role: str):
         """Function to add a member to group
@@ -45,12 +77,12 @@ class Group:
         """
 
         # if adding this player conflicts with desired comp, handle here
-        if len(self.comp[role]['players']) + 1 > self.comp[role]['number']:  # type: ignore
+        if len(self.comp[role]['players']) + 1 > self.comp[role]['number']:
             print(f'{role} already at max capacity!')  # placeholder for now
         
         # else, add the player to the comp list
         else:
-            self.comp[role]['players'].append(member)  # type: ignore
+            self.comp[role]['players'].append(member)
 
     def remove_member(self, member: str):
         """Function to remove a member from group
@@ -60,12 +92,47 @@ class Group:
         """
 
         for role in self.comp:
-            if member in self.comp[role]['players']:  # type: ignore
-                self.comp[role]['players'].pop(self.comp[role]['players'].index(member))  # type: ignore
+            if member in self.comp[role]['players']:
+                self.comp[role]['players'].pop(self.comp[role]['players'].index(member)) 
 
+
+class GroupManager:
+
+    def __init__(self):
+        self.groups = {}
+
+    def add_group(self, leader: str, group: Group):
+        if not self.groups.get(leader):
+            self.groups[leader] = {'groups': []}
+            self.groups[leader]['groups'].append(group)
+        else:
+            self.groups[leader]['groups'].append(group)
+
+    def remove_group(self, leader: str, group_name: str) -> int:
+        if leader in self.groups:
+            groups = [group.name for group in self.groups[leader]['groups']]
+
+            if group_name in groups:
+                self.groups[leader]['groups'].pop(groups.index(group_name))
+                return 1
+        
+        return 0
+
+    def get_groups(self, leader: str, group: str=None) -> List[Group]:
+        if leader in self.groups:
+            groups = [group.name for group in self.groups[leader]['groups']]
+
+            if group and group in groups:
+                return [self.groups[leader]['groups'][groups.index(group)]]
+            elif group and group not in groups:
+                return []
+            else:
+                return self.groups[leader]['groups']
+        
+        return []
 
 if __name__ == '__main__':
-    group = Group('Kyle', '20210321:120000', [1, 3, 6], 'healer')
+    group = Group('Kyle', 'healer')
     group.add_member('Chris', 'dps')
     group.add_member('Natalie', 'tank')
     group.add_member('Kirsten', 'dps')
@@ -75,4 +142,3 @@ if __name__ == '__main__':
     group.add_member('Ryan', 'healer')
 
     print(group.comp)
-    print(group.time)
--- tools/bot.py
+++ tools/bot.py
@@ -1,9 +1,85 @@
+import discord
+
+from .Group import Group, GroupManager
+from .secrets import owner
+
 from discord.ext import commands
-from secrets import owner
 
 bg_bot = commands.Bot(command_prefix='!')
+bg_bot.manager = GroupManager()
 
-@bg_bot.event  # do all this stuff when bot connects successfully
+
+@bg_bot.event
 async def on_ready():
-    bot.owner_id = secrets.owner
-    print(f'Logged in as {bot.user.name}!')
+    # do all this stuff when bot connects successfully
+    bg_bot.owner_id = owner
+    print(f'Logged in as {bg_bot.user.name}!')
+
+
+@bg_bot.command(name='create_group')
+async def create_group(ctx, name: str, role: str, group_type: str=None, comp: str=None, rating: int=None, time: str=None):
+    """Create a new group
+
+    Args:
+        name ([str]): Name of group
+        role ([str]): Role of group leader
+        group_type ([str], optional): <arena2 | arena3 | rbg>
+        comp ([str], optional): Desired tank/healer/dps composition for group i.e. "1 3 6"
+        rating ([int], optional): Desired group rating; if omitted, YOLO
+        time ([str], optional): Desired group start time; if omitted, ASAP
+    """
+
+    owner = ctx.message.author.name
+    comp = [int(i) for i in comp.split()]  # convert string input to array
+
+    new_group = Group(owner, name, role, group_type, rating, time, comp)
+    bg_bot.manager.add_group(owner, new_group)
+    
+    await ctx.send(f'Created new {group_type} group for leader {owner}!')
+
+
+@bg_bot.command(name='delete_group')
+async def delete_group(ctx, group_name: str, owner: str=None):
+    """Remove a group from the GroupManager
+
+    First check to see if the message author has permission to remove the specified group
+
+    Args:
+        group_name ([str]): Name of group to delete
+        owner ([str], optional): Owner of group to delete; if omitted, defaults to message author
+    """
+
+    if owner and owner != ctx.message.author.name:
+        if ctx.message.author.id != bot.owner_id:
+            response = "Sorry, you don't have permission to delete that group. Nerd."
+    else:
+        owner = ctx.message.author.name
+
+    if bg_bot.manager.remove_group(owner, group_name):
+        response = f'{group_name} successfully removed from {owner} groups!'
+    else:
+        response = f'Error in removing {group_name} from {owner} groups!'
+    
+    await ctx.send(response)
+
+
+@bg_bot.command(name='display_group')
+async def display_group(ctx, owner: str, group_name: str=None, option: str=None):
+    """Display the existing groups for specified owner
+
+    Args:
+        owner ([str]): Owner of groups to display
+        group_name ([str], optional): Name of group to display; if omitted, display all groups of specified owner
+        option ([str]), optional): Specify an attribute of Group class to display
+    """
+
+    groups = bg_bot.manager.get_groups(owner, group_name)
+
+    if len(groups) == 0:
+        response = "No groups exist that match those criteria."
+    elif group_name and option:
+        response = [getattr(group, option) for group in groups]
+    else:
+        response = [group.name for group in groups]
+
+    await ctx.send(response)
